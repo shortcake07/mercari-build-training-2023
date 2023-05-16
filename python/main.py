e86154f1dee400910425793e5ecbb03bd26e369b
shortcake07 <itigoshortoishii@gmail.com>
2023-05-16 21:05:57 +0900

    create def items, error hundling and change method of save item

--- python/main.py
+++ python/main.py
@@ -9,7 +9,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 logger = logging.getLogger("uvicorn")
-logger.level = logging.DEBUG
+logger.level = logging.INFO
 images = pathlib.Path(__file__).parent.resolve() / "images"
 origins = [ os.environ.get('FRONT_URL', 'http://localhost:3000') ]
 app.add_middleware(
@@ -20,6 +20,34 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+def save_image(image: UploadFile):
+    image_file = image.file.read()
+    hash_image = hashlib.sha256(image_file).hexdigest()
+    hash_name = hash_image + ".jpg"
+    path = images / hash_name
+    try:
+         with open(path, "wb")as f:
+              f.write(image_file)
+    except FileNotFoundError:
+         return ;
+    return hash_name
+
+def list_items():
+	with open("./items.json", "r")as f:
+            items = json.load(f)
+            return items
+
+def save_item(name:str, category:str, image_filename:str):
+     try:
+          items_data = list_items()
+     except (FileNotFoundError, json.decoder.JSONDecodeError):
+          items_data = {'items':[]}
+
+     item = {'name':name, 'category':category, 'image_filename':image_filename}
+     items_data['items'].append(item)
+     with open("items.json", "w")as f:
+          json.dump(items_data, f)
+
 @app.get("/")
 def root():
     return {"message": "Hello, world!"}
@@ -28,30 +56,25 @@ def root():
 def add_item(name: str = Form(...), category: str = Form(...), image: UploadFile = Form(...)):
     logger.info(f"Receive item: {name}, category: {category}, image:{image.filename}")
     
-    image_file = image.file.read()
-    hash_image = hashlib.sha256(image_file).hexdigest()
-    hash_name = hash_image + ".jpg"
-    path = images / hash_name
-    with open(path, "wb")as f:
-          f.write(image_file)
-
-    receive_items = {'items':[{"name" : name, "category" : category, "image_filename" : hash_name}]}
-    with open("items.json", "w")as f:
-      json.dump(receive_items, f)
- 
+    hash_name = save_image(image)
+    save_item(name, category, hash_name) 
     return {"message": f"item received: {name}"}
 
 @app.get("/items")
-def read_items():
-	with open("items.json", "r")as f:
-		items = json.load(f)
-	return (items)
+def get_items():
+     return list_items()
 
 @app.get("/items/{item_id}")
-def read_certain_items(item_id: int):
-	with open("items.json", "r")as f:
-		items = json.load(f)
-	return (items['items'][item_id - 1])
+def get_item(item_id: int):
+    try:
+         item = list_items()['items'][item_id - 1]
+    except IndexError:
+        return {"message":"item not found"}
+    except ValueError:
+        return {"message":"ValueError"}
+    except FileNotFoundError:
+        return {"message":"FileNotFoundError"}
+    return item
 
 @app.get("/image/{image_filename}")
 async def get_image(image_filename):
